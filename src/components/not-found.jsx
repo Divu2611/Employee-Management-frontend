779b83cae03dba4ffac70f566b0ad4e77c311a9c
Divyesh Chhabra <divusurat26@gmail.com>
2022-08-09 02:51:55 +0530

    Complete Frontend

--- src/components/not-found.jsx
+++ src/components/not-found.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const NotFound = () => {
+    console.log(process.env.REACT_APP_API_URL);
     return (
         <h1 className="mt-5">
             Not Found
         </h1>
     );
 }
- 
+
 export default NotFound;
